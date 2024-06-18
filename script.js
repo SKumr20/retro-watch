a6488e9599ab990eb0870b87fce25b5c3be63d97
Satyam Kumar <113851151+SKumr20@users.noreply.github.com>
2024-06-18 19:24:12 +0530

    Add start function

---
+++ script.js
@@ -0,0 +1,42 @@
+const display = document.getElementById("display");
+let timer = null; // Holds interval of time
+
+let startTime = 0;
+let elapsedTime = 0;
+let isRunning = false; // Flip if watch running
+
+
+function start() {
+    if (!isRunning) {
+        startTime = Date.now() - elapsedTime;
+        timer = setInterval(update, 10); // Update in 10ms
+        isRunning = true;
+    }
+
+}
+
+function stop() {
+
+}
+
+function reset() {
+
+}
+
+function update() {
+    const currentTime = Date.now();
+    elapsedTime = currentTime - startTime; // This is in ms
+
+    let hours = Math.floor(elapsedTime / (1000 * 60 * 60)); // use Florr as we don't want decimal values.
+    let minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
+    let seconds = Math.floor(elapsedTime / 1000 % 60);
+    let milliseconds = Math.floor(elapsedTime % 1000 / 10);
+
+    hours = String(hours).padStart(2, "0");
+    minutes = String(minutes).padStart(2, "0");
+    seconds = String(seconds).padStart(2, "0");
+    milliseconds = String(milliseconds).padStart(2, "0");
+
+    display.textContent = `${hours}:${minutes}:${seconds}:${milliseconds}`;
+
+}
